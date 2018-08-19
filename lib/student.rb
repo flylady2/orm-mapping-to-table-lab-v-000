004e8b1b01b7acc1b6f2430c543883cb513331c2
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-19 20:45:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
 
   def save
     sql = <<-SQL
-    INSERT INTO students(name, grade)
+    INSERT INTO students(id, name, grade)
     VALUES(?, ?)
     SQL
     DB[:conn].execute(sql)
